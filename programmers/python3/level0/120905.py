3d63960386a58decb5ed95ae68c567fba193f064
lave29 <dlaco1028@naver.com>
2023-04-22 23:01:42 +0900

    # n의 배수 고르기

---
+++ programmers/python3/level0/120905.py
@@ -0,0 +1,69 @@
+# n의 배수 고르기
+
+# 문제 설명
+# 정수 n과 정수 배열 numlist가 매개변수로 주어질 때, 
+# numlist에서 n의 배수가 아닌 수들을 제거한 배열을 return하도록 solution 함수를 완성해주세요.
+
+# 제한사항
+# 1 ≤ n ≤ 10,000
+# 1 ≤ numlist의 크기 ≤ 100
+# 1 ≤ numlist의 원소 ≤ 100,000
+# 입출력 예
+# n	numlist	result
+# 3	[4, 5, 6, 7, 8, 9, 10, 11, 12]	[6, 9, 12]
+# 5	[1, 9, 3, 10, 13, 5]	[10, 5]
+# 12	[2, 100, 120, 600, 12, 12]	[120, 600, 12, 12]
+# 입출력 예 설명
+# 입출력 예 #1
+
+# numlist에서 3의 배수만을 남긴 [6, 9, 12]를 return합니다.
+# 입출력 예 #2
+
+# numlist에서 5의 배수만을 남긴 [10, 5]를 return합니다.
+# 입출력 예 #3
+
+# numlist에서 12의 배수만을 남긴 [120, 600, 12, 12]를 return합니다.
+
+# def solution(n, numlist):
+#     answer = []
+#     return answer
+
+# 순서도
+# 1. n의 배수 찾기 → numlist % n == 0 일 경우
+
+n=int(input(''))
+
+numlist = [10, 20, 30, 3, 4, 5]
+
+answer=[]
+for i in numlist :
+    if i % n == 0:
+        #print(i)
+        answer.append(i)
+
+print(answer)
+
+# for i in range(len(numlist)) :
+#     if numlist[i] % n != 0 :
+#         del numlist[i]
+#         print(numlist)
+#     else : 
+#         print(numlist)
+
+# if numlist[2] % 7 != 0 :
+#     del numlist[2]
+#     print(numlist)
+# else : 
+#     print(numlist)
+
+
+# def solution(n, numlist):
+#     answer = []
+    
+#     for i in numlist :
+#         if i % n == 0:
+#             answer.append(i)
+    
+#     return answer
+
+#date 2023.04.22.
