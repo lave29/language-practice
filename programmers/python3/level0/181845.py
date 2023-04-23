58fc09d4bd54ded2cf43ff5e7331dc6fff855a2e
lave29 <dlaco1028@naver.com>
2023-04-23 22:49:59 +0900

    # 문자열로 변환 - 2023.04.23.

---
+++ programmers/python3/level0/181845.py
@@ -0,0 +1,33 @@
+# 문자열로 변환
+
+# 문제 설명
+# 정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+# 제한사항
+# 1 ≤ n ≤ 10000
+# 입출력 예
+# n	result
+# 123	"123"
+# 2573	"2573"
+# 입출력 예 설명
+# 입출력 예 #1
+
+# 123을 문자열로 변환한 "123"을 return합니다.
+# 입출력 예 #2
+
+# 2573을 문자열로 변환한 "2573"을 return합니다.
+
+# def solution(n):
+#     answer = ''
+#     return answer
+
+# 순서도
+# str()을 사용하면 문자열로 반환할 수 있음
+
+
+def solution(n):
+    answer = ''
+    answer = str(n)
+    return answer
+
+#date 2023.04.23.
