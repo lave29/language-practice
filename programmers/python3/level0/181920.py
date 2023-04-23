6ac6fa933c6b45aaca53bc7bd06d3f3c135e0861
lave29 <dlaco1028@naver.com>
2023-04-24 00:05:59 +0900

    # 카운트 업 - 2023.04.23.

---
+++ programmers/python3/level0/181920.py
@@ -0,0 +1,49 @@
+# 카운트 업
+
+# 문제 설명
+# 정수 start와 end가 주어질 때, start부터 end까지의 숫자를 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+# 제한사항
+# 0 ≤ start ≤ end ≤ 50
+# 입출력 예
+# start	end	result
+# 3	10	[3, 4, 5, 6, 7, 8, 9, 10]
+# 입출력 예 설명
+# 입출력 예 #1
+
+# 3부터 10까지의 숫자들을 담은 리스트 [3, 4, 5, 6, 7, 8, 9, 10]를 return합니다.
+
+# def solution(start, end):
+#     answer = []
+#     return answer
+
+# 순서도
+# 1. for문으로 start부터 end까지 넣기
+
+start, end = map(int, input().split())
+
+# answer = []
+
+# for i in range(start, end+1) :
+#     answer.append(i)
+
+# print(answer)
+
+
+
+# def solution(start, end):
+#     answer = []
+#     for i in range(start, end+1) :
+#         answer.append(i)
+#     return answer
+
+
+
+# def solution(start, end):
+#     answer = []
+#     answer = [i for i in range(start, end+1)]
+#     return answer
+
+# print(solution(start, end))
+
+#date 2023.04.23.
