fac777c115ada1de585fb9abe3a679ec9b9d9aef
lave29 <dlaco1028@naver.com>
2023-04-24 17:49:22 +0900

    # 숨어있는 숫자의 덧셈 (1) - 2023.04.24.

---
+++ programmers/python3/level0/120851.py
@@ -0,0 +1,47 @@
+# 숨어있는 숫자의 덧셈 (1)
+
+# 문제 설명
+# 문자열 my_string이 매개변수로 주어집니다. my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+
+# 제한사항
+# 1 ≤ my_string의 길이 ≤ 1,000
+# my_string은 소문자, 대문자 그리고 한자리 자연수로만 구성되어있습니다.
+# 입출력 예
+# my_string	result
+# "aAb1B2cC34oOp"	10
+# "1a2b3c4d123"	16
+# 입출력 예 설명
+# 입출력 예 #1
+
+# "aAb1B2cC34oOp"안의 한자리 자연수는 1, 2, 3, 4 입니다. 따라서 1 + 2 + 3 + 4 = 10 을 return합니다.
+# 입출력 예 #2
+
+# "1a2b3c4d123Z"안의 한자리 자연수는 1, 2, 3, 4, 1, 2, 3 입니다. 따라서 1 + 2 + 3 + 4 + 1 + 2 + 3 = 16 을 return합니다.
+# 유의사항
+# 연속된 숫자도 각각 한 자리 숫자로 취급합니다.
+
+# def solution(my_string):
+#     answer = 0
+#     return answer
+
+# 순서도
+
+
+my_string = input('my_string:')
+
+def solution(my_string):
+    answer = 0
+    for i in my_string :
+        if i == '0' or i == '1' or i == '2' or i == '3' or i == '4' or i == '5' or i == '6' or i == '7' or i == '8' or i == '9' :
+            answer+=int(i)
+
+    return answer
+
+print(solution(my_string))
+
+# 킹받아.....결국 스스로 풀지는 못했다...ㅠㅠ
+# 와우...isdigit()나 try catch 등등 방법이 많다
+# 대박!! 다 아는 함수인데....생각도 못했음;;;
+
+
+# date 2023.04.24.
