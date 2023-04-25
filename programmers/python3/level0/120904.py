786fc97217440bccbfe0be97e5a99ed8fb2e5607
lave29 <dlaco1028@naver.com>
2023-04-25 15:09:31 +0900

    # 숫자 찾기 - 2023.04.25.

---
+++ programmers/python3/level0/120904.py
@@ -0,0 +1,51 @@
+# 숫자 찾기
+
+# 문제 설명
+# 정수 num과 k가 매개변수로 주어질 때, 
+# num을 이루는 숫자 중에 k가 있으면 num의 그 숫자가 있는 자리 수를 return하고 없으면 -1을 return 하도록 solution 함수를 완성해보세요.
+
+# 제한사항
+# 0 < num < 1,000,000
+# 0 ≤ k < 10
+# num에 k가 여러 개 있으면 가장 처음 나타나는 자리를 return 합니다.
+# 입출력 예
+# num	k	result
+# 29183	1	3
+# 232443	4	4
+# 123456	7	-1
+# 입출력 예 설명
+# 입출력 예 #1
+
+# 29183에서 1은 3번째에 있습니다.
+# 입출력 예 #2
+
+# 232443에서 4는 4번째에 처음 등장합니다.
+# 입출력 예 #3
+
+# 123456에 7은 없으므로 -1을 return 합니다.
+
+# def solution(num, k):
+#     answer = 0
+#     return answer
+
+# 순서도
+# num을 이루는 숫자 중에서 k가 있으면 가장 처음 나타나는 자리를 return, 없으면 -1을 return
+
+num = int(input('정수 num:'))
+k = int(input('매개변수 k:'))
+
+def solution(num, k):
+    answer = -1
+
+    for i in str(num) :
+        if i == str(k) :
+            answer = str(num).find(str(k))+1
+    
+    return answer
+
+print(solution(num, k))
+
+# 더럽게 어려웠다....ㅠㅠ
+# enumerate 함수라고 입력으로 받은 데이터와 인덱스 값을 포함하는 객체를 넣어준다고 함
+
+# date 2023.04.25.
